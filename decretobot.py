6bb55cd9cfc0296af2d20372848f0f56a5976e0d
Luiz Irber <luiz.irber@gmail.com>
2017-04-29 17:19:55 -0700

    Carrega frases de um arquivo, adiciona historia de decretos atual

--- decretobot.py
+++ decretobot.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
+import json
 import logging
 from random import sample
 import os
@@ -13,30 +14,20 @@ logger = logging.getLogger(__name__)
 
 
 template = """
-18:08, horário de Brasília. Já pode:
+18:00, horário de Brasília. Já pode:
 
 {frases}
 
 Decreto liberado. Cumpra-se.
 """
 
-permitido = [
-"Beber sidra da semana anterior.",
-"Emendar com um barril de chope que sobrou porque alguém quis calcular as quantidades da festa com base no próprio consumo.",
-"Corote.",
-"Catuaba selvagem.",
-'Lavar o Landau ao som de "Sessenta dias apaixonado - Chitãozinho e Xororó".',
-"Mandar “Deu Onda” pro crush.",
-"Reunir as pontas restantes do Réveillon.",
-"Pedir e pendurar um quibe de procedência duvidosa como o almoço de sábado.",
-"Conversar com o bot do sci-hub em situação de ebriedade.",
-"Chegar em casa às 23:39h do domingo e chegar em casa com um corte de cabelo novo que não lembra quando fez."
-]
+with open('frases.json', 'r') as f:
+    frases = json.load(f)
 
 
 def decretar(bot, update):
     update.message.reply_text(
-        template.format(frases=" ".join(sample(permitido, 5))))
+        template.format(frases=" ".join(sample(frases, 5))))
 
 
 def error(bot, update, error):
